9bb892b86b3860557e06192d412f5f05a63bba49
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2026-01-02 17:49:04 +0400

    opitmized js

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
 let menuToggle = false;
 
-function setMenuToggle() {
-    menuToggle = !menuToggle
-
-    if (menuToggle) {
-        mobileMenu.style.top = "0"
-        headerInner.style.opacity = 0
-        setTimeout(() => {
-            menuBtn.style.display = "none"
-            closeBtn.style.display = "block"
-            headerInner.style.opacity = 1
-            headerInner.style.position = 'fixed'
-
-            main.style.marginTop = headerInner.offsetHeight + "px"
-        }, 700);
-    } else {
-        mobileMenu.style.top = "-150%"
-        menuBtn.style.display = "block"
-        closeBtn.style.display = "none"
-        headerInner.style.position = 'static'
-        main.style.marginTop = 0
-    }
+function toggleMenu() {
+    menuToggle = !menuToggle;
+    mobileMenu.style.top = menuToggle ? "0" : "-150%";
+    headerInner.style.opacity = menuToggle ? 1 : 0;
+    headerInner.style.position = menuToggle ? "fixed" : "static";
+    main.style.marginTop = menuToggle ? headerInner.offsetHeight + "px" : 0;
+    menuBtn.style.display = menuToggle ? "none" : "block";
+    closeBtn.style.display = menuToggle ? "block" : "none";
 }
 
-function setFaqItem(ind) {
-
-    document.getElementsByClassName('faq-answer')[ind].classList.toggle("hidden")
-    const btn = document.getElementsByClassName('faq-toggle')[ind]
-    btn.innerText = btn.innerText == "-" ? "+" : "-"
+function toggleFaqItem(index) {
+    const faqAnswer = document.getElementsByClassName('faq-answer')[index];
+    faqAnswer.classList.toggle("hidden");
+    const faqToggle = document.getElementsByClassName('faq-toggle')[index];
+    faqToggle.innerText = faqToggle.innerText === "-" ? "+" : "-";
 }
 
 const phoneInput = document.querySelector("#phone");
-
 const iti = window.intlTelInput(phoneInput, {
-    initialCountry: "ru",           // стартовая страна
-    separateDialCode: true,         // отдельный код страны слева
+    initialCountry: "ru",
+    separateDialCode: true,
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@24.5.0/build/js/utils.js"
 });
 
 phoneInput.addEventListener("blur", () => {
-    if (phoneInput.value.trim()) {
-        if (!iti.isValidNumber()) {
-            phoneInput.classList.add("invalid");
-        } else {
-            phoneInput.classList.remove("invalid");
-        }
-    }
+    phoneInput.classList.toggle("invalid", phoneInput.value.trim() && !iti.isValidNumber());
 });
